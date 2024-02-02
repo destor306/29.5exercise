cf6a0f5c928d4f4e2045a13b32a76e6bfc9bd8cb
jason <hyunjun306@gmail.com>
2024-02-01 21:35:06 -0500

    this is the fisrt commit

--- makeText.js
+++ makeText.js
@@ -1,2 +1,43 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const process =require('process');
+const {MarkovMachine} = require('./markov');
+const axios = require('axios');
 
+function generateText(data){
+    let result = new MarkovMachine(data);
+    console.log(result.makeText());
+}
+
+function extract(path, format){
+    if (format=='file'){
+        fs.readFile(path, 'utf8', function(err,data){
+            if(err){
+                console.error(`Error reading ${path}: ${err}`)
+                process.exit(1);
+            }
+            
+            console.log(`generated text from ${format} ${path}`)
+            generateText(data);
+        })
+    }
+    else if (format=='url'){
+        webCat(path)
+    }
+    
+}
+
+
+async function webCat(url){
+    try {
+        let resp = await axios.get(url);
+        console.log(`generated text from url ${url}`)
+        generateText(resp.data);
+    }
+    catch(err){
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1);
+    }
+}
+
+extract(process.argv[3], process.argv[2]);
--- markov.js
+++ markov.js
@@ -18,12 +18,36 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    let chains = new Map();
+    
+    for (let i=0;i<this.words.length;i++){
+        let word = this.words[i];
+        let next_word = this.words[i+1] || null;
+        if (chains.has(word)) chains.get(word).push(next_word);
+        else chains.set(word, [next_word])
+    }
+    this.chains = chains;
+    //console.log(this.chains);
   }
 
+  static get_rand(arr){
+    return arr[Math.floor(Math.random()* arr.length)];
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.get_rand(keys);
+    let result=[];
+    
+    while (result.length<numWords && key !== null){
+      result.push(key);
+      key = MarkovMachine.get_rand(this.chains.get(key));
+    }
+  return result.join(' ');
+
   }
 }
+module.exports = {MarkovMachine}
